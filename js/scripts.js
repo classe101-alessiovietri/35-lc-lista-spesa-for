7147c4a5d5316590439f98653a536be6a7c59abb
alessiovietri <info@alessiovietri.it>
2023-06-19 13:09:59 +0200

    LC Completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-console.log('Template HTML CSS JS');
+const listaSpesa = [
+    'qualcosa di buono per Alessio',
+    'uova',
+    'pane',
+    'latte'
+];
+
+const ul = document.getElementById('ul');
+
+for (let index = 0; index < listaSpesa.length; index++) {
+    console.log(index, listaSpesa[index]);
+
+    // const newLi = document.createElement('li');
+    // newLi.innerHTML = listaSpesa[index];
+    // ul.append(newLi);
+
+    // OPPURE
+
+    ul.innerHTML += `
+        <li>
+            ${listaSpesa[index]}
+        </li>
+    `;
+}
+
